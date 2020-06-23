e3c1e41f9a4e42e8473196f72e8a5bf4deae89a7
rkharatmol123 <rkharatmol123@gmail.com>
2020-06-23 13:46:16 +0530

    database

---
+++ autoque.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Community Edition- MySQL GUI v7.01 
+MySQL - 5.5.5-10.1.31-MariaDB : Database - 063autoqa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`063autoqa` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `063autoqa`;
+
+/*Table structure for table `userdetails` */
+
+DROP TABLE IF EXISTS `userdetails`;
+
+CREATE TABLE `userdetails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `dob` varchar(15) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `userdetails` */
+
+insert  into `userdetails`(`id`,`fullname`,`gender`,`mobile`,`email`,`dob`,`address`,`username`,`password`) values (1,'abc','on','7894561230','abc@gmail.com','2019-11-02',NULL,'a','a');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
